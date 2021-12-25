531d4c770f83042b0bdaa0d113fcc64048b49f9b
Jacob Gomez <jakegomez814@hotmail.com>
2021-12-24 22:47:30 -0500

    added help command

---
+++ commands/help.js
@@ -0,0 +1,11 @@
+module.exports = {
+  name: "help",
+  description: "this is a help command",
+  execute(message, args) {
+    message.channel.send(
+      "Sup! I'm Nagatoro, a bot that can do whatever Senpai wants me to do. I don't have a lot of capability yet, but I'm still growing! Here's a list of my current commands:" +
+        //input your commands here
+        "```-test``` ```-help```"
+    );
+  },
+};
--- readme.md
+++ readme.md
@@ -1,3 +1,10 @@
 Sup, Senpai?
 
 This is Nagatoro Bot! AKA Hayase Nagatoro. I'm mainly using this as an excuse to learn Discord bots and mess around. Will post more about this as I go.
+
+current commands:
+
+```
+-test
+-help
+```
