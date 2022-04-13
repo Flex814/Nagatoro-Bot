8ca4dd1f63286ca1f36fd0fbeb749a7b884abdde
Jacob Gomez <jakegomez814@hotmail.com>
2022-04-12 20:04:59 -0400

    restart

--- counters/member-counter.js
+++
@@ -1,9 +0,0 @@
-module.exports = async (client) => {
-  const guild = client.guilds.cache.get("511376020671299589");
-  setInterval(() => {
-    const memberCount = guild.memberCount;
-    const channel = guild.channels.cache.get("924569613491122207");
-    channel.setName(`Total Members: ${memberCount.toLocaleString()}`);
-    console.log(`Updating Member Count`);
-  }, 5000);
-};
